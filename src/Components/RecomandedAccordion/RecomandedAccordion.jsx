ac1239b9cce067c04a64650fc9e6e724270650d9
Ashraful610 <ashalam610@gmail.com>
2024-01-23 23:34:43 +0600

    updated recomandation component

--- src/Components/RecomandedAccordion/RecomandedAccordion.jsx
+++ src/Components/RecomandedAccordion/RecomandedAccordion.jsx
@@ -1,27 +1,8 @@
 import React from "react";
-import {
-  Accordion,
-  AccordionHeader,
-  AccordionBody,
-} from "@material-tailwind/react";
 import Activits from "../Activits/Activits";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
  
-function Icon({ id, open }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={2}
-      stroke="currentColor"
-      className={`${id === open ? "rotate-180" : ""} h-5 w-5 transition-transform`}
-    >
-      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-    </svg>
-  );
-}
  
 const RecomandedAccordion = () =>{
   const [open, setOpen] = React.useState(1);
