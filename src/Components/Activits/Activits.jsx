73474dbc05c7d2bbf077b45f9528a8c0bee21d44
Ashraful610 <ashalam610@gmail.com>
2024-01-24 01:40:04 +0600

    accordion is done

--- src/Components/Activits/Activits.jsx
+++ src/Components/Activits/Activits.jsx
@@ -1,11 +1,103 @@
-
+import React from "react";
+import { ImLocation } from "react-icons/im";
+import { FaCirclePlus, FaUserGroup } from "react-icons/fa6";
+import { RiDeleteBin5Fill } from "react-icons/ri";
    
   const Activits =() =>{
+    const [open, setOpen] = React.useState(1);
+ 
+    const handleOpen = (value) => setOpen(open === value ? 0 : value);
     
-   
     return (
-      <div>
-
+      <div className="w-full">
+        {/* ------- button div --------- */}
+        <div className="flex justify-between">
+          <div className="flex gap-4">
+            <button className={`px-4 py-[7px] border rounded-[100px] ${open === 1 ? 'bg-white font-semibold text-black border-[#EC6274]': 'bg-[#F4F4F4]'}`} onClick={()=>handleOpen(1)}>
+              Morning
+            </button>
+            <button className={`px-4 py-[7px] border rounded-[100px] ${open === 2 ? 'bg-white font-semibold text-black border-[#EC6274]': 'bg-[#F4F4F4]'}`} onClick={()=>handleOpen(2)}>
+              Afternoon
+            </button>
+            <button className={`px-4 py-[7px] border rounded-[100px] ${open === 3 ? 'bg-white font-semibold text-black border-[#EC6274]': 'bg-[#F4F4F4]'}`} onClick={()=>handleOpen(3)}>
+              Dining
+            </button>
+          </div>
+          <button className="flex items-center gap-2">
+            <FaCirclePlus className="text-[#EC6274]"/>
+            <h3 className="text-[#EC6274] underline font-semibold text-lg">Add activity</h3>
+          </button>
+        </div>
+        {/* ---------- activiti card ----------- */}
+        <div className={`w-[387px] h-[294px] rounded-[15px] border mt-[26px] ${open === 1 ? 'block' : 'hidden'}`}>
+          <div className="h-[140px] bg-[url(https://i.ibb.co/R4K43qn/images.jpg)] bg-cover rounded-t-[15px] bg-no-repeat object-cover p-4 relative">
+            <div className="bg-red-500 w-8 h-8 rounded-full text-white flex justify-center items-center absolute right-4">
+              <RiDeleteBin5Fill />
+            </div>
+          </div>
+          <div className="p-4">
+            <div className="h-[74px]">
+              <h2 className="text-lg font-bold mb-2">Alcudia Bay 50-minutes tour from Alcudia</h2>
+              <div className="flex gap-6 ">
+                <p className="text-[#8E8E92] flex gap-2 items-center">
+                  <ImLocation />
+                  Carrer de la Mareperla
+                </p>
+                <p className="text-[#8E8E92] flex gap-2 items-center">
+                  <FaUserGroup />
+                  2 pax
+                </p>
+              </div>
+            </div>
+            <h2 className="text-[25px] font-bold mt-4">USD 344</h2>
+          </div>
+        </div>
+        <div className={`w-[387px] h-[294px] rounded-[15px] border mt-[26px] ${open === 2 ? 'block' : 'hidden'}`}>
+          <div className="h-[140px] bg-[url(https://i.ibb.co/dgXNzCy/images-1.jpg)] bg-cover rounded-t-[15px] bg-no-repeat object-cover p-4 relative">
+            <div className="bg-red-500 w-8 h-8 rounded-full text-white flex justify-center items-center absolute right-4">
+              <RiDeleteBin5Fill />
+            </div>
+          </div>
+          <div className="p-4">
+            <div className="h-[74px]">
+              <h2 className="text-lg font-bold mb-2">Alcudia Bay 50-minutes tour from Alcudia</h2>
+              <div className="flex gap-6 ">
+                <p className="text-[#8E8E92] flex gap-2 items-center">
+                  <ImLocation />
+                  Carrer de la Mareperla
+                </p>
+                <p className="text-[#8E8E92] flex gap-2 items-center">
+                  <FaUserGroup />
+                  3 pax
+                </p>
+              </div>
+            </div>
+            <h2 className="text-[25px] font-bold mt-4">USD 311</h2>
+          </div>
+        </div>
+        <div className={`w-[387px] h-[294px] rounded-[15px] border mt-[26px] ${open === 3 ? 'block' : 'hidden'}`}>
+          <div className="h-[140px] bg-[url(https://i.ibb.co/dtCdVLw/download.jpg)] bg-cover rounded-t-[15px] bg-no-repeat object-cover p-4 relative">
+            <div className="bg-red-500 w-8 h-8 rounded-full text-white flex justify-center items-center absolute right-4">
+              <RiDeleteBin5Fill />
+            </div>
+          </div>
+          <div className="p-4">
+            <div className="h-[74px]">
+              <h2 className="text-lg font-bold mb-2">Alcudia Bay 50-minutes tour from Alcudia</h2>
+              <div className="flex gap-6 ">
+                <p className="text-[#8E8E92] flex gap-2 items-center">
+                  <ImLocation />
+                  Carrer de la Mareperla
+                </p>
+                <p className="text-[#8E8E92] flex gap-2 items-center">
+                  <FaUserGroup />
+                  4 pax
+                </p>
+              </div>
+            </div>
+            <h2 className="text-[25px] font-bold mt-4">USD 510</h2>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Components/RecomandedAccordion/RecomandedAccordion.jsx
+++ src/Components/RecomandedAccordion/RecomandedAccordion.jsx
@@ -10,68 +10,76 @@ const RecomandedAccordion = () =>{
   const handleOpen = (value) => setOpen(open === value ? 0 : value);
  
   return (
-    <>
-     <div>
-        <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 ${open === 1 ? 'rounded-t-[20px]': 'rounded-[20px] mb-6'} `} onClick={()=>handleOpen(1)}>
+    <div className="mb-[180px]">
+      <div>
+          <div className={`h-[72px] flex justify-between items-center border-none px-6 
+          ${open === 1 ? 'rounded-t-[20px] bg-[#F2F2F2]': 'rounded-[20px] mb-6 bg-white'} `} onClick={()=>handleOpen(1)}
+          >
+            <div className="flex items-center gap-[40px]">
+              <h2 className="text-lg font-bold">Day 1</h2>
+              <p className="text-base text-[#8E8E92]">03 June 2023</p>
+            </div>
+            { open === 1 ? <SlArrowUp /> : <SlArrowDown />}
+          </div>
+          {
+            open === 1 && 
+            <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 1 && 'mb-6'}`}>
+              <Activits/>
+            </div>
+          }
+      </div>
+      <div>
+          <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 
+          ${open === 2 ? 'rounded-t-[20px] bg-[#F2F2F2]': 'rounded-[20px] mb-6 bg-white'} `} onClick={()=>handleOpen(2)}
+          >
+            <div className="flex items-center gap-[40px]">
+            <h2 className="text-lg font-bold">Day 2</h2>
+            <p className="text-base text-[#8E8E92]">04 June 2023</p>
+            </div>
+            { open === 2 ? <SlArrowUp /> : <SlArrowDown />}
+          </div>
+          {
+            open === 2 && 
+            <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 2 && 'mb-6'}`}>
+              <Activits/>
+            </div>
+          }
+      </div>
+      <div>
+          <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 
+          ${open === 3 ? 'rounded-t-[20px] bg-[#F2F2F2]': 'rounded-[20px] mb-6 bg-white'} `} onClick={()=>handleOpen(3)}
+          >
           <div className="flex items-center gap-[40px]">
-            <h2 className="text-lg font-bold">Day 1</h2>
-            <p className="text-base text-[#8E8E92]">03 June 2023</p>
+            <h2 className="text-lg font-bold">Day 3</h2>
+            <p className="text-base text-[#8E8E92]">05 June 2023</p>
           </div>
-          { open === 1 ? <SlArrowUp /> : <SlArrowDown />}
-        </div>
-        {
-          open === 1 && 
-          <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 1 && 'mb-6'}`}>
-             <Activits/>
+          { open === 3 ? <SlArrowUp /> : <SlArrowDown />}
           </div>
-        }
-     </div>
-     <div>
-        <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 ${open === 2 ? 'rounded-t-[20px]': 'rounded-[20px] mb-6'} `} onClick={()=>handleOpen(2)}>
-         <div className="flex items-center gap-[40px]">
-          <h2 className="text-lg font-bold">Day 2</h2>
-          <p className="text-base text-[#8E8E92]">04 June 2023</p>
-         </div>
-         { open === 2 ? <SlArrowUp /> : <SlArrowDown />}
-        </div>
-        {
-          open === 2 && 
-          <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 2 && 'mb-6'}`}>
-             <h2>Bangladesh </h2>
+          {
+            open === 3 && 
+            <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 3 && 'mb-6'}`}>
+              <Activits/>
+            </div>
+          }
+      </div>
+      <div>
+          <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 
+            ${open === 4 ? 'rounded-t-[20px] bg-[#F2F2F2]': 'rounded-[20px] mb-6 bg-white'} `} onClick={()=>handleOpen(4)}
+          >
+          <div className="flex items-center gap-[40px]">
+            <h2 className="text-lg font-bold">Day 4</h2>
+            <p className="text-base text-[#8E8E92]">06 June 2023</p>
           </div>
-        }
-     </div>
-     <div>
-        <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 ${open === 3 ? 'rounded-t-[20px]': 'rounded-[20px] mb-6'} `} onClick={()=>handleOpen(3)}>
-         <div className="flex items-center gap-[40px]">
-          <h2 className="text-lg font-bold">Day 3</h2>
-          <p className="text-base text-[#8E8E92]">05 June 2023</p>
-         </div>
-         { open === 3 ? <SlArrowUp /> : <SlArrowDown />}
-        </div>
-        {
-          open === 3 && 
-          <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 3 && 'mb-6'}`}>
-             <h2>Bangladesh </h2>
+          { open === 4 ? <SlArrowUp /> : <SlArrowDown />}
           </div>
-        }
-     </div>
-     <div>
-        <div className={`h-[72px] flex justify-between items-center border-none bg-[#F1F1F1] px-6 ${open === 4 ? 'rounded-t-[20px]': 'rounded-[20px] mb-6'} `} onClick={()=>handleOpen(4)}>
-         <div className="flex items-center gap-[40px]">
-          <h2 className="text-lg font-bold">Day 4</h2>
-          <p className="text-base text-[#8E8E92]">06 June 2023</p>
-         </div>
-         { open === 4 ? <SlArrowUp /> : <SlArrowDown />}
-        </div>
-        {
-          open === 4 && 
-          <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 4 && 'mb-6'}`}>
-             <h2>Bangladesh </h2>
-          </div>
-        }
-     </div>
-    </>
+          {
+            open === 4 && 
+            <div className={`bg-white h-[400px] rounded-b-[20px] p-6 border ${open === 4 && 'mb-6'}`}>
+              <Activits/>
+            </div>
+          }
+      </div>
+    </div>
   );
 }
 
